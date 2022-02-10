745c50a8f03c54b2a17082a7391e231de540946e
Mateo SIndija <mtsindija05@gmail.com>
2022-02-10 18:11:02 +0100

    Added win, lose and millionare message

--- ProjektUrsCpp/main.cpp
+++ ProjektUrsCpp/main.cpp
@@ -22,31 +22,56 @@ struct objectQuestion {
 	objectQuestion(char * question, char * a, char * b, char * c, char * d, uint8_t correctAns): question(question), a(a), b(b), c(c), d(d), correctAns(correctAns) {}
 
 };
-	 const objectQuestion questionsEasy[12] PROGMEM= {
-		 objectQuestion("Najveci planet suncevog sustava je?", "Saturn", "Pluton", "Venera", "Jupiter", 4),
+	 const objectQuestion questionsEasy[10] = {
+		 objectQuestion("Najveci planet suncevog sustava je", "Saturn", "Pluton", "Venera", "Jupiter", 4),
 		 objectQuestion("Sto nije elementarna nepogoda", "Zemljotres", "Pozar", "Glad", "Poplava", 3),
-		 objectQuestion("Ako u splitu pitate koliko je sati i dobijete odgovor 'Po Bota'?", "12:00 sati", "12:30 sati", "13:30 sati", "13:00 sati", 2),
-		 objectQuestion("Sto je celesta?", "Glazbeni  instrument", "Juzno voce", "Orientalni kolac", "Vrsta     pjesme", 1),
-		 objectQuestion("Na kojoj hrvatskoj novcanici se nalazi slika Marka Marulica?", "500", "200", "100", "1000", 1),
-		 objectQuestion("Najveci planet suncevog sustava je?", "Saturn", "Pluton", "Venera", "Jupiter", 4),
-		 objectQuestion("Sto nije elementarna nepogoda", "Zemljotres", "Pozar", "Glad", "Poplava", 3),
-		 objectQuestion("Ako u splitu pitate koliko je sati i dobijete odgovor 'Po Bota'?", "12:00 sati", "12:30 sati", "13:30 sati", "13:00 sati", 2),
-		 objectQuestion("Sto je celesta?", "Glazbeni  instrument", "Juzno voce", "Orientalni kolac", "Vrsta     pjesme", 1),
-		 objectQuestion("Na kojoj hrvatskoj novcanici se nalazi slika Marka Marulica?", "500", "200", "100", "1000", 1),
-		 objectQuestion("h", "Prvi dan  proljeca", "Prvi dan zime", "Prvi dan  ljeta", "Prvi dan  jeseni", 3),
-		 objectQuestion("Which of the following is cotle?", "Klavir", "Saksofon", "Gitaru", "Harmonika", 2),
+		 objectQuestion("Koliko je sati ako je 'Po Bota'", "12:00", "12:30", "13:30", "13:00 ", 2),
+		 objectQuestion("Koji je trenutni HTML standard", "HTML5", "HTML5.5", "HTML4", "HTML3.3", 1),
+		 objectQuestion("Na kojoj hrvatskoj novcanici se nalazi slika Marka Marulica", "500", "200", "100", "1000", 1),
+		 objectQuestion("U kojoj je godini The Godfather prvi put objavljen", "1982", "1972", "1977", "1997", 2),
+		 objectQuestion("Kada je zadnji put hajduk bio prvak HNLa", "2000", "2005", "1999", "2007", 2),
+		 objectQuestion("Koliko igraca ima u olimpijskom timu zacurling", "3", "10", "11", "4", 4),
+		 objectQuestion("Kako je srednje ime Sir Alexa Fergusona", "Mile", "Torbar", "Lord", "Brill ", 2),
+		 objectQuestion("Beirut je glavni grad", "Lebanon", "Bahrain", "Kuwait", "Sirija", 1),
 	 };
 	 
-void checkAnswer(uint8_t *ans, objectQuestion questionsObj, uint8_t *step) {
-	if(*ans == 0) return;
-	if ((*ans) == questionsObj.correctAns) {
-		(*step)++;
-	} else (*step) = 0;
+void displayWin() {
+	display.clrScr();
+	display.setFont(BigFont);
+	display.fillScr(BLUE);
+	display.setBackColor(BLUE);
+	display.print("MILIJUNAS", 80, 110);
+	display.setFont(SmallFont);
+	_delay_ms(3000);
+	display.clrScr();
+} 
+
+void displayCorrect() {
+	display.clrScr();
+	display.setFont(BigFont);
+	display.fillScr(BLUE);
+	display.setBackColor(BLUE);
+	display.print("TOCNO", 110, 110);
+	display.setFont(SmallFont);
+	_delay_ms(1000);
+	display.clrScr();
+}
+
+void displayIncorrect() {
+	display.clrScr();
+	display.setFont(BigFont);
+	display.fillScr(BLUE);
+	display.setBackColor(BLUE);
+	display.print("NETOCNO", 100, 110);
+	display.setFont(SmallFont);
+	_delay_ms(1000);
+	display.clrScr();
 }
 
 void drawScoreHighlight(uint8_t *step) {
+
 	display.setColor(YELLOW);
-	display.fillRoundRect(5 + 50 * (*step % 5), 235, 37 + 50 * (*step % 5), 218);
+	display.fillRoundRect(5 + 50 * ((*step) % 5), 235, 37 + 50 * ((*step) % 5), 218);
 	display.setColor(BLUE);
 }
 
@@ -59,18 +84,19 @@ void displayGUI()
 
 			display.setColor(YELLOW);
 			display.setFont(BigFont);
+			
 			display.print("A:", 10, 100);
 			display.print("B:", 160, 100);
 			display.print("C:", 10, 170);
 			display.print("D:", 160, 170);
-			
-			
+
 			display.setColor(WHITE);
 			display.setFont(SmallFont);
 }
 
 void displayQuestion(objectQuestion questionObj)
 {
+	
 		display.printWithMargin(questionObj.question, 2, 0);
 		display.printWithMargin(questionObj.a, 50, 100, 10);
 		display.printWithMargin(questionObj.b, 200, 100, 10);
@@ -81,36 +107,53 @@ void displayQuestion(objectQuestion questionObj)
 void displayScore(char score[][6], uint8_t *step)
 {
 	
-	if (*step < 5) {
+	if ((*step) < 5) {
 		for (uint8_t i = 0; i < 5; i++) {
-			display.print(score[i], 10 + i * 50, 221);
+			display.print(score[i], 10 + (i * 50), 221);
 			display.print("->", 280, 225);
 		}
 		} else if(*step >= 5 && *step < 10) {
-		for (uint8_t i = 5; i < 10; i++) {
+		for (uint8_t i =5; i < 10; i++) {
 			display.print(score[i], 10 + (i - 5) * 50, 221);
 			display.print("->", 280, 225);
 		}
-		} else {
-		for (uint8_t i = 10; i < 15; i++) {
-			display.print(score[i], 10 + (i - 10) * 50, 221);
-			display.print("->", 280, 225);
-		}
 	}
+}
+
+void checkAnswer(uint8_t *ans, objectQuestion questionsObj, uint8_t *step, char score[][6]) {
+	if((*step) >= 10) {
+		displayWin();
+		*ans = 4;
+	}
+	if((*ans) == 0) return;
+	if ((*ans) == questionsObj.correctAns) {
+			(*step)++;
+			if((*step) < 10) displayCorrect();
+			display.fillScr(BLUE);
+			displayGUI();
+			displayQuestion(questionsEasy[*step]);
+			drawScoreHighlight(step);
+		} 
+		else {
+			if((*step) < 10 ) displayIncorrect();
+			(*step) = 0;
+			display.fillScr(BLUE);
+			displayGUI();
+			displayQuestion(questionsEasy[*step]);
+			drawScoreHighlight(step);
+	};
 }
 
 int main() {
 	
 	uint8_t step = 0;
 	
-	char score[13][6] PROGMEM= {
+	char score[10][6]  = {
 		"100",
-		"300",
 		"500",
-		"1k",
+		"2k",
 		"4k",
 		"16k",
-		"32k",
 		"64k",
 		"125k",
 		"250k",
@@ -121,41 +164,44 @@ int main() {
 	//SET PORTS
 	PORTB = 0x0f;
 	DDRB = 0xf0;
-	
-
 
 	//LCD INIT
+	display.clrScr();
 	display.InitLCD(LANDSCAPE);
 	display.setFont(SmallFont);
-	display.clrScr();
 	display.fillScr(BLUE);
 	display.setBackColor(BLUE);
+	//INIT GUI
+	displayGUI();
+	displayQuestion(questionsEasy[step]);
 	drawScoreHighlight(&step);
-
+	displayScore(score, &step);
+	
 	uint8_t ans = 0;
 
 	while (1) 
 	{
+
 		displayGUI();
 		displayScore(score, &step);
 		
 		if (bit_is_clear(PINB, 0)) {
 			ans = 1;
-			drawScoreHighlight(&step);
+			_delay_ms(300);
 			} else if (bit_is_clear(PINB, 1)) {
 			ans = 2;
-			drawScoreHighlight(&step);
+			_delay_ms(300);
 			} else if (bit_is_clear(PINB, 2)) {
 			ans = 3;
-			drawScoreHighlight(&step);
+			_delay_ms(300);
 			} else if (bit_is_clear(PINB, 3)) {
 			ans = 4;
-			drawScoreHighlight(&step);
+			_delay_ms(300);
 		}
 		
 		// DISPLAY QUESTION AND CHECK ANSWER
-		displayQuestion(questionsEasy[step]);
-		checkAnswer(&ans, questionsEasy[step], &step);
+		
+		checkAnswer(&ans, questionsEasy[step], &step, score);
 		
 		ans = 0;
 	}
